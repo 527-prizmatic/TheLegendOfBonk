b716df330a061d78430a26fec6e532ef962ef0c8
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2023-11-14 14:34:33 +0100

    Map PoC

--- thelegendofbonk/main.c
+++ thelegendofbonk/main.c
@@ -4,6 +4,20 @@
 #include <errno.h>
 #include <math.h>
 
+#include "map.h"
+
 int main() {
+	srand((unsigned int)time(NULL));
+	char tilemap[H_MAP_T][W_MAP_T];
+
+	initMapRandom(tilemap);
+
+	for (int i = 0; i < H_MAP_T; i++) {
+		for (int j = 0; j < W_MAP_T; j++) {
+			printf("%d ", tilemap[i][j]);
+		}
+		printf("\n");
+	}
+
 	return 1;
 }
