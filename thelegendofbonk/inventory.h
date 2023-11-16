44f6f7da209d10a6c716e4db1d41845340103a68
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2023-11-16 08:56:09 +0100

    Why it no compile bru

--- thelegendofbonk/inventory.h
+++ thelegendofbonk/inventory.h
@@ -1,18 +1,15 @@
 #pragma once
 #include "tools.h"
 
+sfSprite* initSprite();
+
 /* Initializes the inventory HUD's textures.
 *  \param _inventorySprite - A sprite object for use as the inventory slots' backdrop
 *  \param _keySprite - A sprite object for use as item icons */
-void initInventory(sfSprite* _inventorySprite, sfSprite* _keySprite);
-
-/* A debug function which allows to manually give oneself items
-*  \param _inventory - An inventory array
-*  \param _keySprite - A sprite object for use as item icons */
-void updateInventory(int* _inventory, sfSprite* _keySprite);
+void initInventory(sfSprite* _inventorySprite, sfSprite* _keySprite, sfRectangleShape* _craftButton, sfText* _craftText);
 
 /* Renders the inventory HUD.
 *  \param _window - A render window
 *  \param _inventorySprite - A sprite object for use as the inventory slots' backdrop
 *  \param _keySprite - A sprite object for use as item icons */
-void displayInventory(sfRenderWindow* _window, sfSprite* _inventorySprite, sfSprite* _keySprite);
+void displayInventory(sfRenderWindow* _window, int* _inventory, sfSprite* _inventorySprite, sfSprite* _keySprite, sfRectangleShape* _craftButton, sfText* _craftText);
