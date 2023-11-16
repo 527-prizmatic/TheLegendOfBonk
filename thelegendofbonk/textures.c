6a5ef0841edb54a4b9f6b7b2d7c026cd98eb1fa9
Prizmatic <66261352+527-prizmatic@users.noreply.github.com>
2023-11-16 17:02:51 +0100

    Finished editor
    'shana'euve

--- thelegendofbonk/textures.c
+++ thelegendofbonk/textures.c
@@ -4,10 +4,11 @@
 
 const sfIntRect t_none = { 0, 0, 0, 0 };
 
-const sfIntRect t_grass = { TS * 1, TS * 4, 48, 48 };
-const sfIntRect t_water = { TS * 5, TS * 8, 48, 48 };
-const sfIntRect t_sand = { TS * 1, TS * 13, 48, 48 };
-const sfIntRect t_rock = { TS * 6, TS * 4, 48, 48 };
+const sfIntRect t_grass			= { TS * 1, TS * 4, 48, 48 };
+const sfIntRect t_water			= { TS * 5, TS * 8, 48, 48 };
+const sfIntRect t_sand			= { TS * 1, TS * 13, 48, 48 };
+const sfIntRect t_rock			= { TS * 6, TS * 4, 48, 48 };
+const sfIntRect t_a			= { TS * 5, TS * 7, 48, 48 };
 
 sfIntRect textureFromId(int _id) {
 	switch (_id) {
@@ -15,6 +16,7 @@ sfIntRect textureFromId(int _id) {
 	case 1: return t_water;
 	case 2: return t_sand;
 	case 3: return t_rock;
+	case 4: return t_a;
 	default: return t_none;
 	}
 }
